70975b1f69d2338b3a4445f81eac19ba944ac55f
valievDM <valievDM@308-3>
2024-12-12 11:02:09 +0800

    Добавьте файлы проекта.

---
+++ WpfApp1/hotelspage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для hotelspage.xaml
+    /// </summary>
+    public partial class hotelspage : Page
+    {
+        public hotelspage()
+        {
+            InitializeComponent();
+            DGridHotels.ItemsSource = praktikEntities.GetContext().Hotel.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            manager.Mainframe.Navigate(new addeditpage());
+        }
+
+        private void BtnEdit_click(object sender, RoutedEventArgs e)
+        {
+            manager.Mainframe.Navigate(new addeditpage());
+        }
+
+        private void BtnAdd_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDelete_Clik(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
